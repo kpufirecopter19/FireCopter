69d74024f4fdd164629cb48c5c531021f3fb4b7c
KueonJaeHwi <kegedokoota@naver.com>
2019-11-04 04:12:45 +0900

    restore repository

---
+++ Project/VR Project002/Assets/Scripts/OculusTouchInput_Helicopter.cs
@@ -0,0 +1,70 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using Valve.VR;
+
+public class OculusTouchInput_Helicopter : MonoBehaviour
+{
+    public SteamVR_Input_Sources leftHand = SteamVR_Input_Sources.LeftHand;
+    public SteamVR_Input_Sources rightHand = SteamVR_Input_Sources.RightHand;
+    public SteamVR_Input_Sources any = SteamVR_Input_Sources.Any;
+
+    // Action Buttons
+    public SteamVR_Action_Pose pose = SteamVR_Actions.default_Pose;
+    public SteamVR_Action_Boolean trigger = SteamVR_Actions.default_Trigger;
+    public SteamVR_Action_Vector2 stick = SteamVR_Actions.default_Stick;
+    public SteamVR_Action_Boolean grab = SteamVR_Actions.default_Grab;
+    public SteamVR_Action_Boolean gripbutton = SteamVR_Actions.default_GripButton;
+    public SteamVR_Action_Boolean ybbutton = SteamVR_Actions.default_YBButton;
+    public SteamVR_Action_Boolean xabutton = SteamVR_Actions.default_XAButton;
+    public SteamVR_Action_Boolean start = SteamVR_Actions.default_StartButton;
+    public float maxSpeed;
+
+    private GameObject helicopter;
+    private GameObject Camera;
+
+    private Vector3 lCtrl;
+    private Vector3 rCtrl;
+
+    private float prevAlt;
+    private float currAlt;
+    private float altDelta;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        altDelta = 0;
+        helicopter = GameObject.Find("Helicopter") ;
+        Camera = GameObject.Find("[CameraRig]");
+
+        lCtrl = pose.GetLocalPosition(leftHand);
+        rCtrl = pose.GetLocalPosition(rightHand);
+        currAlt = lCtrl.y;
+        prevAlt = currAlt;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        currAlt = pose.GetLocalPosition(leftHand).y;
+
+        // 상하 고도 조절 레버
+        if (stick.GetActive(leftHand))
+        {
+            Vector2 movement = stick.GetAxis(leftHand);
+            helicopter.transform.Translate(new Vector3(movement.x, 0, movement.y));
+        }
+        if (gripbutton.GetState(leftHand))
+        {
+            altDelta += (currAlt - prevAlt)/2;
+        }
+        // 전후좌우 조절 레버
+        if (gripbutton.GetState(rightHand))
+        {
+
+        }
+
+        helicopter.transform.Translate(new Vector3(0, altDelta, 0));
+        prevAlt = currAlt;
+    }
+}
---
+++ Project/VR Project002/Assets/Scripts/OculusTouchLaser.cs
@@ -0,0 +1,91 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.EventSystems;
+using Valve.VR;
+
+public class OculusTouchLaser : MonoBehaviour
+{
+    private SteamVR_Behaviour_Pose pose;
+    private SteamVR_Input_Sources hand;
+    private LineRenderer line;
+
+    public SteamVR_Action_Boolean trigger = SteamVR_Actions.default_Trigger;
+
+    public float maxDistance = 30.0f;
+
+    public Color color = Color.blue;
+    public Color clickedColor = Color.green;
+
+    private RaycastHit hit;
+    private Transform tr;
+
+    private GameObject prevObject;
+    private GameObject currObject;
+    // Start is called before the first frame update
+    void Start()
+    {
+        tr = GetComponent<Transform>();
+
+        pose = GetComponent<SteamVR_Behaviour_Pose>();
+        hand = pose.inputSource;
+
+        CreateLineRenderer();
+    }
+
+    void CreateLineRenderer()
+    {
+        line = this.gameObject.AddComponent<LineRenderer>();
+        line.useWorldSpace = false;
+        line.receiveShadows = false;
+
+        line.positionCount = 2;
+        line.SetPosition(0, Vector3.zero);
+        line.SetPosition(1, new Vector3(0, 0, maxDistance));
+
+        line.startWidth = 0.03f;
+        line.endWidth = 0.005f;
+
+        line.material = new Material(Shader.Find("Unlit/Color"));
+        line.material.color = this.color;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if(Physics.Raycast(tr.position,tr.forward,out hit, maxDistance))
+        {
+            line.SetPosition(1, new Vector3(0, 0, hit.distance));
+
+            currObject = hit.collider.gameObject;
+
+            if(currObject != prevObject)
+            {
+                ExecuteEvents.Execute(currObject,
+                    new PointerEventData(EventSystem.current),
+                    ExecuteEvents.pointerEnterHandler);
+                ExecuteEvents.Execute(prevObject,
+                    new PointerEventData(EventSystem.current),
+                    ExecuteEvents.pointerExitHandler);
+                prevObject = currObject;
+            }
+            if (trigger.GetStateDown(hand))
+            {
+                ExecuteEvents.Execute(currObject,
+                    new PointerEventData(EventSystem.current),
+                    ExecuteEvents.pointerClickHandler);
+
+            }
+        }
+        else
+        {
+            if(prevObject != null)
+            {
+                ExecuteEvents.Execute(prevObject,
+                    new PointerEventData(EventSystem.current),
+                    ExecuteEvents.pointerExitHandler);
+                prevObject = null;
+            }
+        }
+    }
+}
